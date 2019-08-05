3c05a68db324614fc9dd2ea369ba7db694a5563b
traticiv <vural.atici@sony.com>
2019-08-05 14:57:01 +0300

    Using variables and examples

--- src/test/java/com/vural/Stepdefs.java
+++ src/test/java/com/vural/Stepdefs.java
@@ -7,30 +7,23 @@ import cucumber.api.java.en.When;
 import static org.junit.Assert.assertEquals;
 
 public class Stepdefs {
-
     private String today;
     private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
+    @Given("^today is \"([^\"]*)\"$")
+    public void today_is(String today) {
+        this.today = today;
     }
 
-    @When("I ask whether it's Friday yet")
+    @When("^I ask whether it's Friday yet$")
     public void i_ask_whether_it_s_Friday_yet() {
-        actualAnswer = IsItFriday.isItFriday(today);
-    }
-
-    @When("^today is Friday$")
-    public void today_is_Friday(){
-        this.today = "Friday";
+        this.actualAnswer = IsItFriday.isItFriday(today);
     }
 
     @Then("^I should be told \"([^\"]*)\"$")
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
     }
-
 }
 
 class IsItFriday {
