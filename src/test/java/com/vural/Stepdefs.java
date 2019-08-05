47205a1c0ffaefe2d09d71d1d6064ea94745e4eb
traticiv <vural.atici@sony.com>
2019-08-05 14:45:32 +0300

    add first sample

---
+++ src/test/java/com/vural/Stepdefs.java
@@ -0,0 +1,40 @@
+package com.vural;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+public class Stepdefs {
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @When("^today is Friday$")
+    public void today_is_Friday(){
+        this.today = "Friday";
+    }
+
+    @Then("^I should be told \"([^\"]*)\"$")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+}
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
